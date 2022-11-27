c816a6281ebea75e221251360d62b35b5fb3b8de
Daniel Baez <daniel2182815@correo.uis.edu.co>
2022-11-27 01:09:02 -0500

    feat(CRUD_usuarios):agregando CRUD usuarios

--- src/main/java/com/example/tareas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/example/tareas/service/impl/UsuarioServiceImpl.java
@@ -1,4 +1,35 @@
 package com.example.tareas.service.impl;
 
-public class UsuarioServiceImpl {
+import com.example.tareas.model.Usuario;
+import com.example.tareas.repository.IUsuarioRepository;
+import com.example.tareas.service.interfaces.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioServiceImpl implements IUsuarioService {
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+    @Override
+    public List<Usuario> mostrarUsuario() {
+        return (List<Usuario>) usuarioRepository.findAll();
+    }
+
+    @Override
+    public Usuario agregarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public Usuario buscarUsuario(Long id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void borrarUsuario(Long id) {
+        usuarioRepository.deleteById(id);
+
+    }
 }
--- src/main/java/com/example/tareas/service/interfaces/IUsuarioService.java
+++ src/main/java/com/example/tareas/service/interfaces/IUsuarioService.java
@@ -1,4 +1,17 @@
 package com.example.tareas.service.interfaces;
 
-public class IUsuarioService {
+import com.example.tareas.model.Usuario;
+import com.example.tareas.service.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface IUsuarioService {
+
+    public List<Usuario> mostrarUsuario();
+    public Usuario agregarUsuario(Usuario usuario);
+    public Usuario buscarUsuario(Long id);
+    public void borrarUsuario(Long id);
 }
