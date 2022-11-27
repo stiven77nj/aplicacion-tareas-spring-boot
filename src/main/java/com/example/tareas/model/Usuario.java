8f9f8f25676da1184e83a074c8de9aa0090fb903
Stiven <stiven77.nj@gmail.com>
2022-11-27 00:19:51 -0500

    feat(creacion):creacion de la entidad usuario

--- src/main/java/com/example/tareas/model/Usuario.java
+++ src/main/java/com/example/tareas/model/Usuario.java
@@ -1,4 +1,38 @@
 package com.example.tareas.model;
 
-public class Usuario {
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table( name = "USUARIOS", schema = "app_tareas")
+public class Usuario implements Serializable {
+
+  @Id
+  @GeneratedValue( strategy = GenerationType.IDENTITY )
+  @Column( name = "ID")
+  private Long id;
+
+  @Column( name = "NOMBRE")
+  private String nombre;
+
+  @Column( name = "APELLIDO")
+  private String apellido;
+
+  @Column( name = "CEDULA")
+  private Long cedula;
+
+  @Column( name = "TELEFONO")
+  private Long telefono;
+
+  @Column( name = "NOMBRE_USUARIO")
+  private String nombreUsuario;
+
+  @Column( name = "FECHA_CREACION")
+  @Temporal( TemporalType.TIMESTAMP )
+  private Date fechaCreacion;
+
 }
